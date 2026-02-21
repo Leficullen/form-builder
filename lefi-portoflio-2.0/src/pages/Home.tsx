64da7ffe0082d28676d74b2f5f9b0040b2444e88
Muh. Alfi Rizqy <lefirizqy2007@gmail.com>
2026-02-21 17:12:33 +0700

    Delete lefi-portoflio-2.0 directory

--- lefi-portoflio-2.0/src/pages/Home.tsx
+++
@@ -1,7 +0,0 @@
-import { motion } from "framer-motion";
-const Home = () => {
-  return (
-    <motion.div className="flex items-center justify-center min-h-[40vh] w-full"></motion.div>
-  );
-};
-export default Home;
--- lefi-portoflio-2.0/src/pages/Projects.tsx
+++
@@ -1,8 +0,0 @@
-const Projects = () => {
-  return (
-    <div className="flex items-center justify-center min-h-[40vh] w-full border border-primary rounded-xl">
-      <h1 className="text-4xl font-bold text-white">Projects Page</h1>
-    </div>
-  );
-};
-export default Projects;
